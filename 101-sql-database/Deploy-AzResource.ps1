1a473390f6efef980f6105ddfaf42a942bb9a8ed
Kiran Patnayakuni <31788728+kpatnayakuni@users.noreply.github.com>
2020-08-04 13:22:05 +0530

    SQL Database

---
+++ 101-sql-database/Deploy-AzResource.ps1
@@ -0,0 +1,66 @@
+[CmdletBinding()]
+param
+(
+    # Resource Group Name
+    [Parameter(Mandatory = $true)]
+    [string] $ResourceGroupName, 
+
+    # Location for all resources.
+    [Parameter(Mandatory = $true)]
+    [string] $Location, 
+
+    # Sql Server Name
+    [Parameter(Mandatory = $false)]
+    [string] $ServerName = -join ('sql', $( -join ((0x30..0x39) + ( 0x61..0x7A) | Get-Random -Count 8 | ForEach-Object { [char]$_ }))),
+
+    # Sql database name
+    [Parameter(Mandatory = $false)]
+    [string] $SqlDBName = 'SampleDB',
+
+    # Administrator credential
+    [Parameter(Mandatory = $true)]
+    [pscredential] $AdminCredential
+)
+
+# Supress the warning messages
+$WarningPreference = [System.Management.Automation.ActionPreference]::SilentlyContinue
+
+# Break the script if the resource group is already exists
+if (Get-AzResourceGroup -Name $ResourceGroupName -ErrorAction SilentlyContinue) 
+{
+    Write-Error -Message "Cannot continue with this deployment since the '$ResourceGroupName' resource group is already exists."
+    return
+}
+
+try 
+{
+    # Create Resource Group
+    $null = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
+
+    # Create a server with a system wide unique server name
+    $SqlServer = New-AzSqlServer -ResourceGroupName $ResourceGroupName -ServerName $ServerName -Location $Location -SqlAdministratorCredentials $AdminCredential
+
+    # Create a blank database with an S0 performance level
+    $DeployStatus = New-AzSqlDatabase  -ResourceGroupName $ResourceGroupName -ServerName $ServerName -DatabaseName $SqlDBName -RequestedServiceObjectiveName "S0" 
+}
+catch
+{
+    # For any reason if the deployment is failed, then rolling it back
+    Write-Host "Execution failed, cleaning the deployment..." -ForegroundColor Red
+    $null = Remove-AzResourceGroup -Name $ResourceGroupName -Force 
+    $DeployStatus = $false
+}
+finally
+{
+    # Display the deployment status
+    if ($DeployStatus) 
+    { 
+        Write-Host -ForegroundColor Green "Deployment is successful!"
+        # Print Sql Server FQDN to connect to the server
+        Write-Host ("Connect SQL Server using {0}:1433" -f $SqlServer.FullyQualifiedDomainName)
+    }
+    else 
+    { 
+        Write-Host -ForegroundColor Red "Deployment is unsuccessful!" 
+    }
+}
---
+++ 101-sql-database/README.md
@@ -0,0 +1,50 @@
+101-sql-database
+===
+Azure SQL Database
+---
+
+## Description
+
+This is a conversion of ARM template [101-sql-database](https://github.com/Azure/azure-quickstart-templates/tree/master/101-sql-database) from the repository [azure\azure-quickstart-templates](https://github.com/Azure/azure-quickstart-templates) to PowerShell Script, and this script will deploy following the resources...
+
+![image](resources.png)
+
+... and in-addition to it, just in-case if the deployment is not successful, then it will rollback the entire deployment.
+
+> ### Note: 
+> If the specified resource group is already exist then the script will not continue with the deployment.
+
+## Syntax
+```
+Deploy-AzResource.ps1 [-ResourceGroupName] <string> [-Location] <string> [[-ServerName] <string>] [[-SqlDBName] <string>] [-AdminCredential] <pscredential> [<CommonParameters>]
+```
+
+## Example
+```powershell
+I 💙 PS> $Credential = Get-Credentials
+
+PowerShell credential request
+Enter your credentials.
+User: sqladmin
+Password for user sqladmin: *************
+
+I 💙 PS> $param = @{
+>> ResourceGroupName = 'azsqldb-rg'
+>> Location = 'westus'
+>> AdminCredential = $Credential
+>> }
+
+I 💙 PS> .\Deploy-AzResources.ps1 @param
+```
+
+## Output
+```
+Deployment is successful!
+Connect SQL Server using sqlo5ckjzma.database.windows.net:1433
+```
+
+> Azure Cloud Shell comes with Azure PowerShell pre-installed and you can deploy the above resources using Cloud Shell as well.
+>
+>[![](https://shell.azure.com/images/launchcloudshell.png "Launch Azure Cloud Shell")](https://shell.azure.com)
+
+Thank you.
